4335c531f59f43c7901c58408eef3b2d70dfc386
John Eisenmann <esperantanaso@gmail.com>
2018-12-25 09:30:55 -0500

    Draw at different zoom levels

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -26,6 +26,15 @@ var colorSet = [
     new Color(255, 255, 255),
     new Color(64, 64, 64)
 ];
+var colorStringSet;
+
+colorStringSet = [];
+var index = 0;
+while (index < colorSet.length) {
+    var tempColor = colorSet[index];
+    colorStringSet.push(tempColor.toString());
+    index += 1;
+}
 
 function addGetInitializationInfoCommand() {
     gameUpdateCommandList.push({
@@ -68,6 +77,16 @@ function getChunk(pos) {
     return chunkMap[tempKey];
 }
 
+function setTileSize(size) {
+    tileSize = size;
+    canvasTileWidth = canvasWidth / tileSize;
+    canvasTileHeight = canvasHeight / tileSize;
+    imageData = context.createImageData(canvasTileWidth, canvasTileHeight);
+    imageDataList = imageData.data;
+    tileCanvas.width = canvasTileWidth;
+    tileCanvas.height = canvasTileHeight;
+}
+
 function Chunk(pos, tileData) {
     this.pos = roundPosToChunk(pos);
     this.tileList = [];
@@ -85,7 +104,7 @@ Chunk.prototype.getTile = function(pos) {
     return this.tileList[(pos.x - this.pos.x) + (pos.y - this.pos.y) * chunkSize];
 }
 
-Chunk.prototype.draw = function() {
+Chunk.prototype.getDrawBounds = function() {
     var tempStartPos = new Pos(
         Math.max(cameraPos.x, this.pos.x),
         Math.max(cameraPos.y, this.pos.y),
@@ -94,11 +113,21 @@ Chunk.prototype.draw = function() {
         Math.min(cameraPos.x + canvasTileWidth, this.pos.x + chunkSize),
         Math.min(cameraPos.y + canvasTileHeight, this.pos.y + chunkSize),
     );
+    return [tempStartPos, tempEndPos];
+}
+
+Chunk.prototype.drawPixelLayer = function() {
+    var tempBounds = this.getDrawBounds();
+    var tempStartPos = tempBounds[0];
+    var tempEndPos = tempBounds[1];
     var tempPos = tempStartPos.copy();
     while (tempPos.y < tempEndPos.y) {
         tempPos.x = tempStartPos.x;
         while (tempPos.x < tempEndPos.x) {
             var tempTile = this.getTile(tempPos);
+            if (tempTile == tileSet.DIAMOND && tileSize > 30) {
+                tempTile = tileSet.EMPTY;
+            }
             var tempColor = colorSet[tempTile];
             var index = (
                 (tempPos.x - cameraPos.x)
@@ -113,18 +142,41 @@ Chunk.prototype.draw = function() {
     }
 }
 
+Chunk.prototype.drawShapeLayer = function() {
+    var tempBounds = this.getDrawBounds();
+    var tempStartPos = tempBounds[0];
+    var tempEndPos = tempBounds[1];
+    var tempPos = tempStartPos.copy();
+    while (tempPos.y < tempEndPos.y) {
+        tempPos.x = tempStartPos.x;
+        while (tempPos.x < tempEndPos.x) {
+            var tempTile = this.getTile(tempPos);
+            if (tempTile == tileSet.DIAMOND && tileSize > 30) {
+                var tempColorString = colorStringSet[tempTile];
+                context.fillStyle = tempColorString;
+                var tempOffsetX = (tempPos.x - cameraPos.x) * tileSize;
+                var tempOffsetY = (tempPos.y - cameraPos.y) * tileSize;
+                context.beginPath();
+                context.moveTo(tempOffsetX + tileSize / 2, tempOffsetY + tileSize / 8);
+                context.lineTo(tempOffsetX + tileSize * 7 / 8, tempOffsetY + tileSize / 2);
+                context.lineTo(tempOffsetX + tileSize / 2, tempOffsetY + tileSize * 7 / 8);
+                context.lineTo(tempOffsetX + tileSize / 8, tempOffsetY + tileSize / 2);
+                context.lineTo(tempOffsetX + tileSize / 2, tempOffsetY + tileSize / 8);
+                context.fill();
+            }
+            tempPos.x += 1;
+        }
+        tempPos.y += 1;
+    }
+}
+
 function ClientDelegate() {
     
 }
 
 ClientDelegate.prototype.initialize = function() {
     context.imageSmoothingEnabled = false;
-    canvasTileWidth = canvasWidth / tileSize;
-    canvasTileHeight = canvasHeight / tileSize;
-    imageData = context.createImageData(canvasTileWidth, canvasTileHeight);
-    imageDataList = imageData.data;
-    tileCanvas.width = canvasTileWidth;
-    tileCanvas.height = canvasTileHeight;
+    setTileSize(64);
     addGetInitializationInfoCommand();
 }
 
@@ -138,24 +190,40 @@ ClientDelegate.prototype.addCommandsBeforeUpdateRequest = function() {
 }
 
 ClientDelegate.prototype.timerEvent = function() {
+    var tempBackgroundColor = colorSet[6];
     var index = 0;
     while (index < imageDataList.length) {
-        imageDataList[index] = 64;
-        imageDataList[index + 1] = 64;
-        imageDataList[index + 2] = 64;
+        imageDataList[index] = tempBackgroundColor.r;
+        imageDataList[index + 1] = tempBackgroundColor.g;
+        imageDataList[index + 2] = tempBackgroundColor.b;
         imageDataList[index + 3] = 255;
         index += 4;
     }
     var tempChunk = getChunk(new Pos(0, 0));
     if (tempChunk !== null) {
-        tempChunk.draw();
+        tempChunk.drawPixelLayer();
+        tileContext.putImageData(imageData, 0, 0);
+        context.drawImage(tileCanvas, 0, 0, canvasWidth, canvasHeight);
+        tempChunk.drawShapeLayer();
     }
-    tileContext.putImageData(imageData, 0, 0);
-    context.drawImage(tileCanvas, 0, 0, canvasWidth, canvasHeight);
 }
 
 ClientDelegate.prototype.keyDownEvent = function(keyCode) {
-    
+    if (focusedTextInput !== null) {
+        return true;
+    }
+    if (keyCode == 49) {
+        setTileSize(64);
+    }
+    if (keyCode == 50) {
+        setTileSize(32);
+    }
+    if (keyCode == 51) {
+        setTileSize(16);
+    }
+    if (keyCode == 52) {
+        setTileSize(8);
+    }
     return true;
 }
 
