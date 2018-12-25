05c65c70b3d35a49489367a6ba7084a7dac102bb
John Eisenmann <esperantanaso@gmail.com>
2018-12-25 12:23:05 -0500

    Draw player entity

--- public/javascript/game.js
+++ public/javascript/game.js
@@ -27,6 +27,16 @@ var colorSet = [
     new Color(64, 64, 64)
 ];
 var colorStringSet;
+var playerEntityList = [];
+var playerQuarterOutline = [
+    new Pos(1/2, 0),
+    new Pos(3/4, 1/4),
+    new Pos(1, 1/4),
+    new Pos(1, 1),
+    new Pos(1/4, 1),
+    new Pos(1/4, 3/4),
+    new Pos(0, 1/2)
+]
 
 colorStringSet = [];
 var index = 0;
@@ -87,6 +97,14 @@ function setTileSize(size) {
     tileCanvas.height = canvasTileHeight;
 }
 
+function getTile(pos) {
+    var tempChunk = getChunk(pos);
+    if (tempChunk === null) {
+        return null;
+    }
+    return tempChunk.getTile(pos);
+}
+
 function Chunk(pos, tileData) {
     this.pos = roundPosToChunk(pos);
     this.tileList = [];
@@ -116,6 +134,13 @@ Chunk.prototype.getDrawBounds = function() {
     return [tempStartPos, tempEndPos];
 }
 
+function drawPixel(posX, posY, color) {
+    var index = (posX + posY * canvasTileWidth) * 4;
+    imageDataList[index] = color.r;
+    imageDataList[index + 1] = color.g;
+    imageDataList[index + 2] = color.b;
+}
+
 Chunk.prototype.drawPixelLayer = function() {
     var tempBounds = this.getDrawBounds();
     var tempStartPos = tempBounds[0];
@@ -129,13 +154,7 @@ Chunk.prototype.drawPixelLayer = function() {
                 tempTile = tileSet.EMPTY;
             }
             var tempColor = colorSet[tempTile];
-            var index = (
-                (tempPos.x - cameraPos.x)
-                + (tempPos.y - cameraPos.y) * canvasTileWidth
-            ) * 4;
-            imageDataList[index] = tempColor.r;
-            imageDataList[index + 1] = tempColor.g;
-            imageDataList[index + 2] = tempColor.b;
+            drawPixel(tempPos.x - cameraPos.x, tempPos.y - cameraPos.y, tempColor);
             tempPos.x += 1;
         }
         tempPos.y += 1;
@@ -152,8 +171,7 @@ Chunk.prototype.drawShapeLayer = function() {
         while (tempPos.x < tempEndPos.x) {
             var tempTile = this.getTile(tempPos);
             if (tempTile == tileSet.DIAMOND && tileSize > 30) {
-                var tempColorString = colorStringSet[tempTile];
-                context.fillStyle = tempColorString;
+                context.fillStyle = colorStringSet[tempTile];
                 var tempOffsetX = (tempPos.x - cameraPos.x) * tileSize;
                 var tempOffsetY = (tempPos.y - cameraPos.y) * tileSize;
                 context.beginPath();
@@ -161,12 +179,115 @@ Chunk.prototype.drawShapeLayer = function() {
                 context.lineTo(tempOffsetX + tileSize * 7 / 8, tempOffsetY + tileSize / 2);
                 context.lineTo(tempOffsetX + tileSize / 2, tempOffsetY + tileSize * 7 / 8);
                 context.lineTo(tempOffsetX + tileSize / 8, tempOffsetY + tileSize / 2);
-                context.lineTo(tempOffsetX + tileSize / 2, tempOffsetY + tileSize / 8);
                 context.fill();
             }
             tempPos.x += 1;
         }
         tempPos.y += 1;
+    }
+}
+
+function PlayerEntity(pos) {
+    this.pos = pos;
+    this.isInFront = true;
+    this.direction = 1;
+    playerEntityList.push(this);
+}
+
+PlayerEntity.prototype.drawQuarter = function(offsetX, offsetY, flipX, flipY) {
+    var tempTilePos = this.pos.copy();
+    tempTilePos.x += offsetX;
+    tempTilePos.y += offsetY;
+    var tempBasePosX = tempTilePos.x - cameraPos.x;
+    var tempBasePosY = tempTilePos.y - cameraPos.y;
+    var tempTile = getTile(tempTilePos);
+    var tempColorIndex;
+    if (tempTile == tileSet.FRONT_AND_BACK) {
+        tempColorIndex = 6;
+    } else if (this.isInFront) {
+        if (tempTile == tileSet.BACK) {
+            tempColorIndex = tileSet.FRONT_AND_BACK;
+        } else {
+            tempColorIndex = tileSet.FRONT;
+        }
+    } else {
+        if (tempTile == tileSet.FRONT) {
+            tempColorIndex = tileSet.FRONT_AND_BACK;
+        } else {
+            tempColorIndex = tileSet.BACK;
+        }
+    }
+    if (tileSize <= 30) {
+        var tempColor = colorSet[tempColorIndex];
+        drawPixel(tempBasePosX, tempBasePosY, tempColor);
+        return;
+    }
+    var tempColorString = colorStringSet[tempColorIndex];
+    context.fillStyle = tempColorString;
+    context.beginPath();
+    var index = 0;
+    while (index < playerQuarterOutline.length) {
+        var tempOutlinePos = playerQuarterOutline[index];
+        var tempPosX = tempBasePosX;
+        var tempPosY = tempBasePosY;
+        if (flipX) {
+            tempPosX += 1 - tempOutlinePos.x;
+        } else {
+            tempPosX += tempOutlinePos.x;
+        }
+        if (flipY) {
+            tempPosY += 1 - tempOutlinePos.y;
+        } else {
+            tempPosY += tempOutlinePos.y;
+        }
+        tempPosX *= tileSize;
+        tempPosY *= tileSize;
+        if (index <= 0) {
+            context.moveTo(tempPosX, tempPosY);
+        } else {
+            context.lineTo(tempPosX, tempPosY);
+        }
+        index += 1;
+    }
+    context.fill();
+}
+
+PlayerEntity.prototype.drawAllQuarters = function() {
+    this.drawQuarter(0, 0, false, false);
+    this.drawQuarter(1, 0, true, false);
+    this.drawQuarter(0, 1, false, true);
+    this.drawQuarter(1, 1, true, true);
+}
+
+PlayerEntity.prototype.drawPixelLayer = function() {
+    this.drawAllQuarters();
+}
+
+PlayerEntity.prototype.drawShapeLayer = function() {
+    this.drawAllQuarters();
+    if (tileSize > 30) {
+        var tempPos = this.pos.copy();
+        tempPos.subtract(cameraPos);
+        tempPos.scale(tileSize);
+        context.fillStyle = colorStringSet[5];
+        var tempOffsetX;
+        if (this.direction > 0) {
+            tempOffsetX = tileSize * 3 / 8;
+        } else {
+            tempOffsetX = 0;
+        }
+        context.fillRect(
+            tempPos.x + tileSize / 2 + tempOffsetX,
+            tempPos.y + tileSize * 5 / 8,
+            tileSize / 8,
+            tileSize * 3 / 8
+        );
+        context.fillRect(
+            tempPos.x + tileSize + tempOffsetX,
+            tempPos.y + tileSize * 5 / 8,
+            tileSize / 8,
+            tileSize * 3 / 8
+        );
     }
 }
 
@@ -183,6 +304,7 @@ ClientDelegate.prototype.initialize = function() {
 ClientDelegate.prototype.setLocalPlayerInfo = function(command) {
     // TEST CODE.
     addGetChunkCommand(new Pos(0, 0));
+    new PlayerEntity(new Pos(10, 10));
 }
 
 ClientDelegate.prototype.addCommandsBeforeUpdateRequest = function() {
@@ -202,9 +324,23 @@ ClientDelegate.prototype.timerEvent = function() {
     var tempChunk = getChunk(new Pos(0, 0));
     if (tempChunk !== null) {
         tempChunk.drawPixelLayer();
-        tileContext.putImageData(imageData, 0, 0);
-        context.drawImage(tileCanvas, 0, 0, canvasWidth, canvasHeight);
+    }
+    var index = 0;
+    while (index < playerEntityList.length) {
+        var tempPlayerEntity = playerEntityList[index];
+        tempPlayerEntity.drawPixelLayer();
+        index += 1;
+    }
+    tileContext.putImageData(imageData, 0, 0);
+    context.drawImage(tileCanvas, 0, 0, canvasWidth, canvasHeight);
+    if (tempChunk !== null) {
         tempChunk.drawShapeLayer();
+    }
+    var index = 0;
+    while (index < playerEntityList.length) {
+        var tempPlayerEntity = playerEntityList[index];
+        tempPlayerEntity.drawShapeLayer();
+        index += 1;
     }
 }
 
